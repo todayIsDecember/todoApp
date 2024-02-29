239c6d3d67e2d34f3e0bd70d71cabc3665075a54
todayIsDecember <timkivit@gmail.com>
2024-02-29 02:11:38 +0200

    add mobile desctop

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
+        protocol: "https",
+        hostname: "todoserver-onv1.onrender.com",
       },
     ],
   },
