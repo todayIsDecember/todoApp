f4177d8fd993ceddcae5584e5857545da2564099
todayIsDecember <timkivit@gmail.com>
2024-03-01 07:04:36 +0200

    change config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "todoserver-1.onrender.com",
+        hostname: "todoserver-52sw.onrender.com",
       },
     ],
   },
